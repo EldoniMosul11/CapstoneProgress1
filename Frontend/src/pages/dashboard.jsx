b7da171b5a40ba666fb0ff40943a921a35f9826b
FarrelllAdityaaa <farrelpaksiaditya@gmail.com>
2025-11-23 23:28:09 +0700

    ubah keterangan di pie chart

--- Frontend/src/pages/dashboard.jsx
+++ Frontend/src/pages/dashboard.jsx
@@ -410,7 +410,7 @@ export default function Beranda() {
                         <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
                     </svg>
                     <p className="text-sm font-medium text-gray-700">
-                        Proporsi produk terjual dalam akumulasi <span className="font-bold text-blue-600">4 minggu terakhir</span> (sesuai audit).
+                        Proporsi produk terjual dalam akumulasi <span className="font-bold text-blue-600">4 minggu terakhir</span>.
                     </p>
                 </div>
             </div>
